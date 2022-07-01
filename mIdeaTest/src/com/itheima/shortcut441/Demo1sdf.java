25e96791e3cb8c24c64d882938293cc2f65d552f
ze <1@gmail.com>
2022-07-01 17:25:58 +0800

    Initial commit

---
+++ mIdeaTest/src/com/itheima/shortcut441/Demo1sdf.java
@@ -0,0 +1,18 @@
+package com.itheima.shortcut441;
+
+//import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Demo1sdf {
+    public static void main(String[] args) {
+//        StringBuilder str = new StringBuilder();
+        long start = System.currentTimeMillis();
+        String str = new String();
+        for (int i = 0; i < 100000; i++) {
+            str += i;
+        }
+//        System.out.println(str);
+        System.out.println(System.currentTimeMillis() - start);
+    }
+}
---
+++ mIdeaTest/src/com/itheima/shortcut441/Demo2.java
@@ -0,0 +1,11 @@
+package com.itheima.shortcut441;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 5; i++) {
+            System.out.println(i);
+        }
+        int a = 10;
+        byte b = (byte) a;
+    }
+}
