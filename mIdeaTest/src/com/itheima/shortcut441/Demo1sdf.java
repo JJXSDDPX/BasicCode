65e1526614d25c65c416a59c85b5f6a9d6a64e38
ze <1@gmail.com>
2022-07-01 17:35:00 +0800

    第一次提交

--- mIdeaTest/src/com/itheima/shortcut441/Demo1sdf.java
+++ mIdeaTest/src/com/itheima/shortcut441/Demo1sdf.java
@@ -8,7 +8,8 @@ public class Demo1sdf {
     public static void main(String[] args) {
 //        StringBuilder str = new StringBuilder();
         long start = System.currentTimeMillis();
-        String str = new String();
+        String str = "sdaf";
+//        str = str;
         for (int i = 0; i < 100000; i++) {
             str += i;
         }
